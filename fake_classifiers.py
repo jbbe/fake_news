1bec5748c56a3d6a9b340d7d10ce347847e84c3f
jbbe <jbbe@umich.edu>
2019-07-29 10:49:44 -0400

    plotting roc-auc scores added

--- fake_classifiers.py
+++ fake_classifiers.py
@@ -1,22 +1,24 @@
 import pandas as pd
 import numpy as np
 
+from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score, precision_score, accuracy_score, recall_score
+from sklearn.metrics import roc_auc_score, precision_score, accuracy_score, recall_score, roc_curve
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
 from sklearn.utils import shuffle
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.pipeline import FeatureUnion, Pipeline
 from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
 
 
 df = shuffle(pd.read_csv('news_dataset.csv').dropna())
 # Fake = 1
 df['label'] = np.where(df['label']=='fake',1,0)
-n = 3000
+n = 1000
 print('Data size', n)
 df = df[:n]
 # print(df.columns)
@@ -176,12 +178,6 @@ def log_pipe():
                                                   validate=False)),
                             ('tfidf',
                               TfidfVectorizer(ngram_range=(2,4)))])),
-                # ('publication_count',
-                #   Pipeline([('extract_field',
-                #               FunctionTransformer(lambda x: x['title'],
-                #                                   validate=False)),
-                #             ('countvectorizer',
-                #               CountVectorizer())])),
                 ('content_tfidf',
                   Pipeline([('extract_field',
                               FunctionTransformer(lambda x: x['content'],
@@ -204,45 +200,90 @@ def gridsearch_over_log():
   print(grid_search.best_params_)
 
 def classifier_pipe():
+  stops = stopwords.words('english')
   transformer = [('FeatureUnion',FeatureUnion([
                 ('title_tfidf',
                   Pipeline([('extract_field',
                               FunctionTransformer(lambda x: x['title'],
                                                   validate=False)),
                             ('tfidf',
-                              TfidfVectorizer(ngram_range=(2,4)))])),
-                # ('publication_count',
-                #   Pipeline([('extract_field',
-                #               FunctionTransformer(lambda x: x['title'],
-                #                                   validate=False)),
-                #             ('countvectorizer',
-                #               CountVectorizer())])),
+                              TfidfVectorizer(ngram_range=(1,3), lowercase=False, stop_words=stops))])),
                 ('content_tfidf',
                   Pipeline([('extract_field',
                               FunctionTransformer(lambda x: x['content'],
                                                   validate=False)),
                             ('tfidf',
-                              TfidfVectorizer(min_df=3, ngram_range=(3,5)))])),]))]
+                              TfidfVectorizer(min_df=3, ngram_range=(1,9), lowercase=False, stop_words=stops))])),
+                              ]))]
 
-  transformer.append(('clf',LogisticRegression(solver='liblinear')))                    
+  transformer.append(('clf',LogisticRegression(solver='sag', n_jobs=-1, C=100, max_iter=8000)))                    
   return Pipeline(transformer)
 
 def general_gridsearch():
   pipe = classifier_pipe()
-  print(pipe.get_params().keys())
-  param_grid = dict(FeatureUnion__content_tfidf__tfidf__min_df=[3, 5], clf = [SVC(C=1000), LogisticRegression(C=1000)])
+  
+  # print(pipe.get_params().keys())
+  param_grid = dict(clf__C=[1,100,1000])
+  # param_grid = dict(FeatureUnion__content_tfidf__tfidf__ngram_range=[(1,9)])
+  # param_grid = dict(clf__solver=['liblinear','sag','newton-cg','lbfgs'])
+                      # FeatureUnion__content_tfidf__tfidf__lowercase=[True,False],
   grid_search = GridSearchCV(pipe, param_grid=param_grid, cv=2)
   grid_search.fit(X_train, y_train)
+  print(pipe)
   print("score = %3.2f" %(grid_search.score(X_test, y_test)))
   print(grid_search.best_params_)
 
+  y_score = grid_search.decision_function(X_test)
+  fpr, tpr, thresholds = roc_curve(y_test, y_score)
+  roc_auc = roc_auc_score(y_test, y_score)
+  predictions = grid_search.predict(X_test)
+  print('roc_auc:', roc_auc)
+  print('precision:',precision_score(y_test, predictions))
+  print('recall:',recall_score(y_test, predictions))
+  # plt.figure(figsize=(16, 12))
+  # plt.plot(fpr, tpr, label='ROC curve (area = %0.3f)' % roc_auc)
+  # plt.plot([0, 1], [0, 1], 'k--')
+  # plt.xlim([0.0, 1.0])
+  # plt.ylim([0.0, 1.0])
+  # plt.xlabel('False Positive Rate (1 - Specificity)', size=16)
+  # plt.ylabel('True Positive Rate (Sensitivity)', size=16)
+  # plt.title('ROC Curve', size=20)
+  # plt.legend(fontsize=14)
+  # plt.show()
 
 # print('log with pipe roc, precision, recall:', log_reg_with_pipe())
 # gridsearch_over_log()
 general_gridsearch()
 # print(log_pipe().get_params().keys())
 
-
+"""
+TODO
+refine list of stopwords
+understand what a roc list is
+map all words to a pos tag
+then run tfidf/countvectorizer on ngrams of mapping
+maybe try sentence trees?
+sentence lengths?
+"""
 
 # Precision oriented minimizes false positives -- labeling real as fake
 # Recall oriented minimizes false negatives-- labeling fake as real
+
+"""
+['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", 
+"you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 
+'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 
+'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those',
+ 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 
+ 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 
+ 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 
+ 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 
+ 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 
+ 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 
+ 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 
+ 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 
+ "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', 
+ "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 
+ 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't",
+  'won', "won't", 'wouldn', "wouldn't"]
+"""
